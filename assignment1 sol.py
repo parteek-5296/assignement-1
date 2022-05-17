e9d81a4db6d154ff1728b5fe76d37121150623bd
parteek-5296 <105700137+parteek-5296@users.noreply.github.com>
2022-05-17 11:45:55 +0530

    Add files via upload

---
+++ assignment1 sol.py
@@ -0,0 +1,30 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\lenovo\Desktop\New folder\assignment1.py ==========
+============ QUESTION 1 ===========
+Enter First Number: 50
+Enter Second number: 53
+Enter Third number: 56
+The average is : 53.0
+============ QUESTION 2 ===========
+Please enter your income: 500000
+Please enter number of dependents: 4
+Total Payable tax is:  95600.0
+============ QUESTION 3 ===========
+Enter your SID: 21102070
+Enter your name: Parteek Badwal
+Enter your Gender(M/F/U): M
+Enter your Course: Btech
+Enter your cgpa: 8.0
+[21102070, 'Parteek Badwal', 'M', 'Btech', 8.0]
+============ QUESTION 4 ===========
+Marks of 1st student : 50
+Marks of 2nd student : 60
+Marks of 3rd student : 70
+Marks of 4th student : 40
+Marks of 5th student : 54
+The list of marks of students is :  [50, 60, 70, 40, 54]
+============ QUESTION 5 ===========
+Updated list:  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'purple', 'Yellow']
